74f3c5d9b6fe1397e5413497cb87fce898f20b8a
nguyentri61 <nguyenphanminhtri612004@gmail.com>
2025-03-20 22:13:28 +0700

    ddd

--- APP/app/src/main/java/com/example/app/MainActivity.java
+++ APP/app/src/main/java/com/example/app/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.intro);
+        setContentView(R.layout.activity_main);
 
 
         AnhXa();
@@ -81,7 +81,5 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("logg", t.getMessage());
             }
         });
-
-
     }
 }
